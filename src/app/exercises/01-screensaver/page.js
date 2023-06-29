97bdb436c3e05e447852ebb28f7a412f9e98818e
robloche <robloche@gmail.com>
2023-06-29 11:41:59 +0000

    Update page.js

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-import ScreenSaver from '../../../components/ScreenSaver';
+const COLORS = ["lavender", "slateblue", "hotpink", "white"];
+
+console.log("page 1");
 
 function ScreenSaverExercise() {
+  console.log("page 2");
+
+  return <div>foo</div>;
+  /*
   return (
     <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+      <div>Choose your color:</div>
+      <ul>
+        {COLORS.map((color) => (
+          <li>
+            <Link href={`/exercises/01-screensaver/${color}`}>{color}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
+  */
 }
 
 export default ScreenSaverExercise;
